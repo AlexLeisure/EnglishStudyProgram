a72618e57efd34a158c650905c8b52d4c5da5bfe
Alex <alex.leisure125@gmail.com>
2020-03-11 23:31:38 -0500

    Added more content to book/test classes, and tested making a JSON file

--- EnglishStudyProgram/src/BookTestContent.cs
+++ EnglishStudyProgram/src/BookTestContent.cs
@@ -5,6 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EnglishStudyProgram {
+	class BookInfo {
+		public int BookID { get; set; }
+		public string KeyInfo { get; set; }
+		public List<string> InfoList { get; set; }
+
+		public BookInfo(int bookID, string keyInfo, List<string> infoList) {
+			BookID = bookID;
+			KeyInfo = keyInfo;
+			InfoList = infoList;
+		}
+	}
 	class BookTestContent {
+		public List<BookInfo> TestContent { get; set; }
+		public BookTestContent() {
+			TestContent = new List<BookInfo>();
+		}
+
+		public void AddTestContent(int bookID, string keyInfo, List<string> infoList) {
+			BookInfo bookInfo = new BookInfo(bookID, keyInfo, infoList);
+			TestContent.Add(bookInfo);
+		}
+
+		public BookInfo GetBookInfo(int index) {
+			BookInfo bookInfo = null;
+			try {
+				bookInfo = TestContent.ElementAt(index);
+			} catch(ArgumentNullException) {
+				Console.WriteLine("Tried to pass in a null index...");
+			} catch(ArgumentOutOfRangeException) {
+				Console.WriteLine("Tried to access an index out of range: index=" + index);
+			}
+			return bookInfo;
+		}
+
 	}
 }
--- EnglishStudyProgram/src/Program.cs
+++ EnglishStudyProgram/src/Program.cs
@@ -6,26 +6,32 @@ using System.Threading.Tasks;
 using System.Text.Json;
 
 namespace EnglishStudyProgram {
-	class BookInfo {
-		public int bookID { get; set; }
-		public string keyInfo { get; set; }
-		public List<string> infoList { get; set; }
-
-		public BookInfo(int bookID, string keyInfo, List<string> infoList) {
-			this.bookID = bookID;
-			this.keyInfo = keyInfo;
-			this.infoList = infoList;
-		}
-
-		public string getJson() {
-			string json = "";
-
-			return json;
-		}
-	}
 
 	class Program {
 		static void Main(string[] args) {
+			Console.Write("Press anything to start program:");
+			Console.Read();
+			TestMenu();
+			Console.Write("Press anything to exit...");
+			Console.Read();
+		}
+
+		static void TestMenu() {
+			BookTestContent testContent = new BookTestContent();
+			List<string> infoList = new List<string>();
+			infoList.Add("First bullet point");
+			infoList.Add("Second bullet point");
+			infoList.Add("Third bullet point");
+			testContent.AddTestContent(0, "This is a test question", infoList);
+
+			var options = new JsonSerializerOptions {
+				WriteIndented = true,
+			};
+
+			string jsonString = JsonSerializer.Serialize(testContent, options);
+			Console.WriteLine(jsonString);
+			System.IO.File.WriteAllText("../../json/testingData.json", jsonString);
+
 		}
 	}
 }
