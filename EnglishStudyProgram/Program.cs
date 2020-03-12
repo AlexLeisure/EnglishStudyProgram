686b67955951abd9c6f686a9b7734eef408935d9
Alex <alex.leisure125@gmail.com>
2020-03-11 22:29:15 -0500

    Added System.Text.json to project

--- EnglishStudyProgram/Program.cs
+++ EnglishStudyProgram/Program.cs
@@ -3,8 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace EnglishStudyProgram {
+	class BookInfo {
+		public int bookID { get; set; }
+		public string keyInfo { get; set; }
+		public List<string> infoList { get; set; }
+
+		public BookInfo(int bookID, string keyInfo, List<string> infoList) {
+			this.bookID = bookID;
+			this.keyInfo = keyInfo;
+			this.infoList = infoList;
+		}
+
+		public string getJson() {
+			string json = "";
+
+			return json;
+		}
+	}
+
 	class Program {
 		static void Main(string[] args) {
 		}
